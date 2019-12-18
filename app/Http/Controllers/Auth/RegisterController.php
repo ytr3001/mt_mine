e6fbe4a7f70155018ac5a028f8d9894008226db1
ytr3001 <ytr3001@gmail.com>
2019-12-18 14:40:42 +0900

    RegisterController修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/register_done';
+    protected $redirectTo = '/auth/register_done';
 
     /**
      * Create a new controller instance.
